a65dc81f09980c5350ba3c72044cf72b1dcb3c18
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-09-19 22:15:36 -0300

    Update v1.0.7
    - Section 8: Complete

---
+++ 8_NODE_E_MYSQL/5_edicao_primeira_etapa/index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+const hbs = exphbs.create({
+    partialsDir: ["views/partials"]
+})
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine','handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    res.render('home');
+
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+    const title = req.body.title;
+    const pagesqty = req.body.pagesqty;
+
+    const query = `INSERT INTO books (title, page_number) VALUES ("${title}", ${pagesqty})`
+
+    conn.query(query, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.redirect('/books');
+    })
+
+})
+
+app.get('/books', (req, res) => {
+
+    const query = "SELECT * FROM books";
+
+    conn.query(query, function (err, data) {
+    
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        const books = data;
+
+        res.render('books', { books })
+
+    })
+
+})
+
+app.get('/books/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    const query = `SELECT * FROM books WHERE id=${id}`
+
+    conn.query(query, function (err, data) {
+
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        const book = data[0];
+
+        res.render('book', { book })
+
+    })
+
+})
+
+app.get('/books/edit/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    const query = `SELECT * FROM books WHERE id = ${id}`;
+
+    conn.query(query, function (err, data) {
+
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        const book = data[0];
+
+        res.render('editbook', { book })
+
+    })
+
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password : '',
+    database: 'nodemysql'
+})
+
+conn.connect(function(err) {
+
+    if (err) {
+        console.log(err)
+        return;
+    }
+
+    console.log('Successful connection to MySQL')
+
+})
+
+
+app.listen(3000, () => {
+    console.clear();
+    console.log('The application is online on port 3000')
+
+})
